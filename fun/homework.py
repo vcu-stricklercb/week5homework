37f8ba3093deef8e550aef35cbb1ed68fa8a2982
vcu-stricklercb <78453263+vcu-stricklercb@users.noreply.github.com>
2021-03-08 22:58:28 -0500

    Update homework.py

--- fun/homework.py
+++ fun/homework.py
@@ -4,36 +4,26 @@ def find_greatest_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the largest number in the list.
     """
-incoming_list = [1,2,3,4,5]
-incoming_list.sort()
-greatest_number = incoming_list[-1]
-print(greatest_number)
-pass
+    greatest_number = max(incoming_list)
+    return greatest_number
 def find_least_number(incoming_list):
     """
     Required parameter, incoming_list, should be a list.
     Find the smallest/least number in the list.
     """
-incoming_list = [1,2,3,4,5]
-incoming_list.sort()
-smallest_number = incoming_list[:1]
-print(smallest_number)
-pass
+    smallest_number = min(incoming_list)
+    return smallest_number
 def add_list_numbers(incoming_list):
     """
     Required parameter, incoming_list, should be a list.
     Add all the values together and return it.
     """
-incoming_list = [1,2,3,4,5]
-Addlist = sum(incoming_list)
-print(Addlist)
-pass
-def add_list_numbers(incoming_list):
+    Addlist = sum(incoming_list)
+    return Addlist
+def longest_value_key(incoming_dict):
     """
-    Required parameter, incoming_list, should be a list.
-    Add all the values together and return it.
+    Required parameter, incoming_dict, should be a dict.
+    Find the KEY that has a value with the highest length, use the len() function
     """
-incoming_dict = ['blue', 'green', 'black', 'orange', 'yellow', 'burgundy']
-largest_word = max(incoming_dict, key = len)
-print(largest_word)
-pass
+    largest_word = max(incoming_dict, key = len)
+    return largest_word
