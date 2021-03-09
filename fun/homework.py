61675c60839a29e2e03f662f9d4557a5cc008817
vcu-stricklercb <78453263+vcu-stricklercb@users.noreply.github.com>
2021-03-08 19:13:05 -0500

    Update homework.py

--- fun/homework.py
+++ fun/homework.py
@@ -6,7 +6,11 @@ def find_greatest_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the largest number in the list.
     """
-    pass
+incoming_list = None
+incoming_list.sort()
+greatest_number = incoming_list[-1] 
+print(greatest_number)
+pass
 
 
 def find_least_number(incoming_list):
@@ -14,7 +18,12 @@ def find_least_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the smallest/least number in the list.
     """
-    pass
+incoming_list = None
+incoming_list.sort()
+smallest_number = incoming_list[:1] 
+print(smallest_number)
+pass
+
 
 
 def add_list_numbers(incoming_list):
@@ -22,13 +31,18 @@ def add_list_numbers(incoming_list):
     Required parameter, incoming_list, should be a list.
     Add all the values together and return it.
     """
+incoming_list = None
+Addlist = sum(incoming_list)
+print(Addlist)  
+pass
 
-    pass
 
-
-def longest_value_key(incoming_dict):
+def add_list_numbers(incoming_list):
     """
-    Required parameter, incoming_dict, should be a dict.
-    Find the KEY that has a value with the highest length, use the len() function
+    Required parameter, incoming_list, should be a list.
+    Add all the values together and return it.
     """
-    pass
+incoming_dict = None
+largest_word = max(incoming_dict, key = len)
+print(largest_word)  
+
